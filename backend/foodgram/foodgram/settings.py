085b28a9cee39f34c1560b76af9a06af5578d9a2
Ilnurr <Radikovissh@yandex.ru>
2023-07-14 23:13:58 +0300

    finakl

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -104,11 +104,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'collect_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
