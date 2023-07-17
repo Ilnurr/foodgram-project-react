6334fc7e5e28e1372eed40efca1b86a87d6ba9ea
Ilnurr <Radikovissh@yandex.ru>
2023-07-17 04:13:36 +0300

    fdf

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -1,24 +1,24 @@
 from recipes.models import ShoppingList
 
 
-def create_shopping_list_file(user): 
-    shopping_list = ShoppingList.objects.filter(user=user) 
-    ingredients = {} 
-    for item in shopping_list: 
-        for ingredient in item.recipe.ingredients.all(): 
-            if hasattr(ingredient, 'amount'): 
-                if ingredient.name in ingredients: 
-                    ingredients[ingredient.name] += ingredient.amount 
-                else: 
-                    ingredients[ingredient.name] = ingredient.amount 
-            else: 
-                if hasattr(ingredient, 'ingredien'): 
-                    if ingredient.name in ingredients: 
-                        ingredients[ingredient.name] += ingredient.ingredien 
-                    else: 
-                        ingredients[ingredient.name] = ingredient.ingredien 
+def create_shopping_list_file(user):
+    shopping_list = ShoppingList.objects.filter(user=user)
+    ingredients = {}
+    for item in shopping_list:
+        for ingredient in item.recipe.ingredients.all():
+            if hasattr(ingredient, 'amount'):
+                if ingredient.name in ingredients:
+                    ingredients[ingredient.name] += ingredient.amount
+                else:
+                    ingredients[ingredient.name] = ingredient.amount
+            else:
+                if hasattr(ingredient, 'ingredien'):
+                    if ingredient.name in ingredients:
+                        ingredients[ingredient.name] += ingredient.ingredien
+                    else:
+                        ingredients[ingredient.name] = ingredient.ingredien
  
-    output = '' 
-    for name, amount in ingredients.items(): 
-        output += f'{name} - {amount} {name.measurement_unit}\n' 
+    output = ''
+    for name, amount in ingredients.items():
+        output += f'{name} - {amount} {name.measurement_unit}\n'
     return output
